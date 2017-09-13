f339da25c38e56e436dcc48cfaeab0f214c40f3d
Aurelie-feyzin <gramusset.a@gmail.com>
2017-09-13 11:43:32 +0200

    add faq.php

---
+++ faq.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BugBurger : FAQ </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<nav>
+
+</nav>
+<header>
+    <nav>
+        <img src="http://via.placeholder.com/800x50" class="img-responsive" alt="Sandwich" width="800" height="50">
+    </nav>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>FAQ</h1>
+            <p class="hidden-xs">Nos réponses aux questions le plus souvent posées.</p>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h3>Je suis allergique aux crustacés, puis-je manger vos produits?
+                </h3>
+            </div>
+            <div class="col-xs-12 text-center">
+                <p>Attention les insectes contiennent de la “chitine” dans leur carapace qui est l’élément déclencheur
+                    d’allergie chez les crustacés. Ne mangez pas d’insectes si vous êtes allergiques aux crustacés..</p>
+            </div>
+        </div>
+        <div class="row text-center ">
+            <div class="col-xs-12">
+                <h3>Je suis allergique aux crustacés, que puis-je manger chez vous?</h3>
+            </div>
+            <div class="col-xs-12">
+                <p>Pour partager un moment de dégustation avec vos amis, nous pouvons proposer de remplacer nos steaks
+                    d’insectes par du Tofu.</p>
+            </div>
+        </div>
+
+        <div class="row text-center ">
+            <div class="col-xs-12">
+                <h3>Vos steaks d’insectes sont-ils gluants?</h3>
+            </div>
+            <div class="col-xs-12">
+                <p>Pas du tout, nous assurons que la texture et le goût sont très proches de viandes dites “standardes”.
+                    Les steaks de criquets ressemblent à de la viande de boeuf et les steaks aux vers de farine
+                    ressemblent à du porc.</p>
+            </div>
+        </div>
+
+        <div class="row text-center ">
+            <div class="col-xs-12">
+                <h3>Est ce que je peux modifier le contenu d’un Burger si je le souhaite?</h3>
+            </div>
+            <div class="col-xs-12">
+                <p>Bien sûr, en revanche nos recettes sont très réfléchies et nous ne pouvons que vous conseiller les
+                    recettes proposées..</p>
+            </div>
+        </div>
+        <div class="row text-center ">
+            <div class="col-xs-12">
+                <h3>Vos produits sont ils vraiment régionaux?</h3>
+            </div>
+            <div class="col-xs-12">
+                <p>A l’exception de nos insectes produits à La Loupe tous nos produits proviennent d’agriculteurs à
+                    moins de 30 km de nos restaurants. La provenance est affichée dans chacunes de nos enseignes..</p>
+            </div>
+        </div>
+        <hr>
+        <?php include "footer.php"; ?>
+</header>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</body>
+</html>
