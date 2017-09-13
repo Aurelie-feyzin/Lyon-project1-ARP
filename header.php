2347d0e9851905eab223192e3fe95c415f559a0e
Pierrick Reux <pierrick.reux@gmail.com>
2017-09-13 16:51:34 +0200

    add header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">BugBurger</a>
+            </div>
+
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#concept">Concept <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#nosrestaurants">Nos Restaurants</a></li>
+                    <li><a href="#notreequipe"></a></li>
+                    <li><a href="#METTRE LE LIEN FAQ">FAQ</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
