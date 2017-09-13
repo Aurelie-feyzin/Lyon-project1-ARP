91dea8106cc97bd8033fe4acfaf82eaa990909d5
Aurelie-feyzin <gramusset.a@gmail.com>
2017-09-13 18:30:34 +0200

    add map.php

---
+++ map.php
@@ -0,0 +1,47 @@
+<section id="nosrestaurants">
+    <div class="row text-center">
+        <div class="col-xs-12">
+            <h2>Nos Restaurants</h2>
+        </div>
+
+    </div>
+
+    <div class="row"></div>
+    <div class="col-xs-12 col-md-6">
+        <div id="city_map">
+            <iframe src="https://www.google.com/maps/d/embed?mid=13Z09XYBII_Le5JSJRmqoWNg3CGA" width="640" height="480"></iframe>
+        </div>
+    </div>
+    <div class="hidden-xs col-md-4 col-md-offset-1">
+
+        <dl id="listRestau" class="">
+            <dt class="place">Bug Burger La Loupe</dt>
+            <dd>18 Rue de la gare, 28240 La Loupe</dd>
+
+            <dt class="place"> Bug Burger Lyon</dt>
+            <dd>17 rue Delandine, 69002 Lyon</dd>
+
+            <dt class="place"> Bug Burger Bordeaux</dt>
+            <dd>15 Place Canteloup, 33800 Bordeaux</dd>
+
+            <dt class="place">Bug Burger Lille  </dt>
+            <dd>Euratechnologies, 4 avenue des Saules, 59160 Lomme</dd>
+
+            <dt class="place"> Bug Burger Orléans </dt>
+                        <dd>Le Lab’O, 1 avenue du Champ de Mars, 45000 Orléans</dd>
+
+            <dt class="place">  Bug Burger Paris  </dt>
+            <dd>11 rue de Poissy, 75005 Paris</dd>
+
+            <dt class="place"> Bug Burger Bordeaux  </dt>
+            <dd> 1 Rue des Corps-de-Garde, 67100 Strasbourg</dd>
+
+            <dt class="place"> Bug Burger Toulouse  </dt>
+            <dd>1 place de la Bourse, 31000 Toulouse</dd>
+
+        </dl>
+
+    </div>
+
+</section>
+
