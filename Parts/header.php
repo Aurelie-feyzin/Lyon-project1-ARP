75c6f5d72b590d98a40922e2df4757c9d27c770a
Aurelie-feyzin <gramusset.a@gmail.com>
2017-09-20 22:04:00 +0200

    update link

--- Parts/header.php
+++ Parts/header.php
@@ -8,18 +8,18 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">BugBurger</a>
+                <a class="navbar-brand" href="../index.php">BugBurger</a>
             </div>
 
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="../index.php#concept">Concept <span class="sr-only">(current)</span></a></li>
-                    <li><a href="../index.php#menu">Menu</a></li>
-                    <li><a href="../index.php#nosrestaurants">Nos Restaurants</a></li>
-                    <li><a href="../index.php#notreequipe"></a></li>
-                    <li><a href="../faq.php">FAQ</a></li>
-                    <li><a href="../contact.php">Contact</a></li>
+                    <li><a class="scrollTo" href="../index.php#menu">Menu</a></li>
+                    <li><a class="scrollTo" href="../index.php#nosrestaurants">Nos Restaurants</a></li>
+                    <li><a  href="../team.php"></a></li>
+                    <li><a  href="../faq.php">FAQ</a></li>
+                    <li><a  href="../forms.php">Contact</a></li>
                 </ul>
             </div>
         </div>
