599b8bd3f5a17a2a470ab295b649da342936c13b
Aurelie-feyzin <gramusset.a@gmail.com>
2017-09-20 21:06:24 +0200

    scroll on map only click first

--- Parts/map.php
+++ Parts/map.php
@@ -8,9 +8,8 @@
 <div class="row">
     <div class="col-xs-12">
         <div id="city_map" class="embed-responsive embed-responsive-16by9">
-            <iframe class="center-block" src="https://www.google.com/maps/d/embed?mid=13Z09XYBII_Le5JSJRmqoWNg3CGA"
-                    width="640"
-                    height="480"></iframe>
+            <iframe class="center-block scrolloff" src="https://www.google.com/maps/d/embed?mid=13Z09XYBII_Le5JSJRmqoWNg3CGA"
+                    width="640"  height="480" scrolling ="no" ></iframe>
         </div>
     </div>
 </div>
